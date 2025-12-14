a5b90f8b840481b034faac0b056b27cc693b2f2d
Merishva J <merishva.j.1@gmail.com>
2025-12-14 21:48:45 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (50.61%) - LeetSync

--- 2491-smallest-even-multiple/smallest-even-multiple.java
+++ 2491-smallest-even-multiple/smallest-even-multiple.java
@@ -1,15 +1,5 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
-        int p;
-        int g;
-        if(n%2==0){
-            p=n*n;
-            g=p;
-        }
-        else{
-            g=n*2;
-            n=g;
-        }
-        return n;
+        return (n%2==0)?n:n*2;
     }
 }
